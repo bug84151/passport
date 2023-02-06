cebb30ad0c83f0a73cfb4270d8248bd5faf576b1
Adoyi Godspower <bug84151@gmail.com>
2023-02-06 01:35:27 +0100

    secoond commit

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const whole_faq = document.querySelectorAll(".whole_faq");
 const answer = document.querySelectorAll(".answer");
+const header = document.querySelector(".header");
 const menu_icon = document.querySelector(".menu_icon");
 const nav_bar = document.querySelector(".nav_bar");
 const x_mark = document.querySelector(".x_mark");
@@ -12,10 +13,12 @@ whole_faq.forEach((faq) => {
 
 menu_icon.addEventListener("click", () => {
   nav_bar.classList.toggle("mobile_nav");
+  header.classList.toggle("ipad_nav");
   menu_icon.classList.toggle("close");
 });
 
 x_mark.addEventListener("click", () => {
   nav_bar.classList.toggle("mobile_nav");
+  header.classList.toggle("ipad_nav");
   menu_icon.classList.toggle("close");
 });
